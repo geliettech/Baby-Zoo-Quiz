d6245fe348e9a79e120956c9fb7b068cb98f6941
julietoge <julietogechi27@gmail.com>
2023-08-10 15:43:31 +0100

    updated

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -1,41 +1,23 @@
 const questions = [
-  // Objective question
+  // Objective questions
   {
     type: "objective",
     question: "What is the capital of France?",
     options: ["Paris", "Berlin", "Rome", "Madrid"],
     answer: 0,
   },
-   // Subjective question
-   {
-    type: "subjective",
-    question: "Describe the process of cellular respiration.",
-    answer:
-      "Cellular respiration is the process by which cells convert glucose and oxygen into energy, carbon dioxide, and water.",
-  },
-  // Subjective question
   {
-    type: "subjective",
-    question: "Explain the concept of gravity.",
-    answer: "Gravity is the force that attracts two masses toward each other.",
-  },
-   // Objective question
-   {
     type: "objective",
     question: "Which planet is known as the Red Planet?",
     options: ["Venus", "Mars", "Jupiter", "Saturn"],
     answer: 1,
   },
-   // Subjective question
-   {
-    type: "subjective",
-    question: "Discuss the significance of the Industrial Revolution.",
-    answer:
-      "The Industrial Revolution marked a major shift in manufacturing and transportation, leading to significant social and economic changes.",
+  {
+    type: "objective",
+    question: "What is the chemical symbol for gold?",
+    options: ["Au", "Ag", "Fe", "Cu"],
+    answer: 0,
   },
-  
-   
-   // Objective question
   {
     type: "objective",
     question: "Who painted the Mona Lisa?",
@@ -47,7 +29,6 @@ const questions = [
     ],
     answer: 1,
   },
-   // Objective question
   {
     type: "objective",
     question: "Which gas do plants use for photosynthesis?",
@@ -55,10 +36,24 @@ const questions = [
     answer: 0,
   },
 
-
-   
-   
-    // Subjective question
+  // Subjective questions
+  {
+    type: "subjective",
+    question: "Explain the concept of gravity.",
+    answer: "Gravity is the force that attracts two masses toward each other.",
+  },
+  {
+    type: "subjective",
+    question: "Describe the process of cellular respiration.",
+    answer:
+      "Cellular respiration is the process by which cells convert glucose and oxygen into energy, carbon dioxide, and water.",
+  },
+  {
+    type: "subjective",
+    question: "Discuss the significance of the Industrial Revolution.",
+    answer:
+      "The Industrial Revolution marked a major shift in manufacturing and transportation, leading to significant social and economic changes.",
+  },
   {
     type: "subjective",
     question:
@@ -66,14 +61,6 @@ const questions = [
     answer:
       "Natural selection is the process through which species evolve by individuals with advantageous traits being more likely to survive and reproduce.",
   },
-  // Objective question
-  {
-    type: "objective",
-    question: "What is the chemical symbol for gold?",
-    options: ["Au", "Ag", "Fe", "Cu"],
-    answer: 0,
-  },
-    // Subjective question
   {
     type: "subjective",
     question: "What are the key principles of democracy?",
@@ -83,13 +70,10 @@ const questions = [
 ];
 let currentQuestion = 0;
 let score = 0;
-let timeLeft = 1800; // 30 minutes in seconds
 
-const timer = document.getElementById("timer");
 const questionContainer = document.getElementById("question-container");
 const nextButton = document.getElementById("next-btn");
 const scoreContainer = document.getElementById("score-container");
-
 function showQuestion() {
   const question = questions[currentQuestion];
   questionContainer.innerHTML = `
@@ -111,25 +95,6 @@ function showQuestion() {
     }
 	`;
 }
-
-function updateTimer() {
-	const minutes = Math.floor(timeLeft / 60);
-	const seconds = timeLeft % 60;
-	timer.textContent = `Time Remaining: ${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  }
-  
-  function startTimer() {
-	const timerInterval = setInterval(() => {
-	  if (timeLeft > 0) {
-		timeLeft--;
-		updateTimer();
-	  } else {
-		clearInterval(timerInterval);
-		showScore();
-	  }
-	}, 1000);
-  }
-
 function checkAnswer() {
 	const question = questions[currentQuestion];
 	if (question.type === 'objective') {
@@ -163,5 +128,4 @@ function showScore() {
 }
 nextButton.addEventListener("click", checkAnswer);
 
-startTimer();
 showQuestion();
